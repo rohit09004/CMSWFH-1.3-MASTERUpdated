5057a41b6c1507b76312d8f80cbed546c429cb67
106-3249 <106-3249@JCI-MH68.JCI-SG.local>
2023-06-15 14:49:16 +0530

    webdriver setting change using dependancy

--- src/main/java/com/csm/qa/base/CSMBase.java
+++ src/main/java/com/csm/qa/base/CSMBase.java
@@ -13,6 +13,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import com.csm.qa.util.TestUtil;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class CSMBase {
 	
 	public static WebDriver driver;
@@ -39,14 +41,23 @@ public class CSMBase {
 	public static void initialize() throws InterruptedException
 	{
 	 
-		//System.setProperty("webdriver.chrome.driver","D:\\Eclipse_rohit\\EclipseSetup_Library\\ChromeDriverJune2023\\chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver","C:\\ChromeDriverJune2023\\chromedriver.exe");
+//		//System.setProperty("webdriver.chrome.driver","D:\\Eclipse_rohit\\EclipseSetup_Library\\ChromeDriverJune2023\\chromedriver.exe");
+//		System.setProperty("webdriver.chrome.driver","C:\\ChromeDriverJune2023\\chromedriver.exe");
+//		
+//		
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--remote-allow-origins=*");
+//		//driver=new ChromeDriver();
+//		 driver = new ChromeDriver(options);
+//		 //driver = new ChromeDriver();
 		
+		WebDriverManager.chromedriver().setup();
+		//driver=new ChromeDriver();
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
-		driver = new ChromeDriver(options);
-
-		 driver.manage().window().maximize();
+        driver = new ChromeDriver(options);
+		 
+         driver.manage().window().maximize();
 		 driver.manage().deleteAllCookies();
 //		 driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(TestUtil.pageload_wait));
 //		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TestUtil.implicit_wait));
