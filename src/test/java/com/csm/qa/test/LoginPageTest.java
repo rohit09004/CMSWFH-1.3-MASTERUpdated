453abd4a81dd59b5721429ac93de34af84bcd0c3
106-3249 <106-3249@JCI-MH68.JCI-SG.local>
2023-06-19 11:37:27 +0530

    Aapachee POM dependencies added

--- src/test/java/com/csm/qa/test/LoginPageTest.java
+++ src/test/java/com/csm/qa/test/LoginPageTest.java
@@ -16,15 +16,17 @@ import com.csm.qa.pages.LoginPage;
 public class LoginPageTest extends CSMBase{
 	LoginPage loginPage;
 	HomePage homePage;
-	public static Sheet sh;
 	
-//	public void excelDataImport() throws EncryptedDocumentException, IOException {
+//	 @BeforeTest
+//       public void excelDataImport() throws EncryptedDocumentException, IOException {
 //		
 //		FileInputStream file =new FileInputStream("D:\\Eclipse_rohit\\PO_Screen_Data.xlsx");
-//		sh= WorkbookFactory.create(file).getSheet("Sheet");
-////		String shipNo=sh.getRow(3).getCell(2).getStringCellValue();
-////	    driver.findElement(By.xpath("/html/body/div/div/section[2]/div[1]/form/div[3]/div/div/div/div[6]/div/div/div[1]/div/table/tbody/tr[1]/td[2]/div/div/div/input")).sendKeys(shipNo);
-//    }
+//		Sheet sh= WorkbookFactory.create(file).getSheet("Sheet");
+//		//String shipNo=sh.getRow(1).getCell(1).getStringCellValue();
+//	    //driver.findElement(By.xpath("/html/body/div/div/section[2]/div[1]/form/div[3]/div/div/div/div[6]/div/div/div[1]/div/table/tbody/tr[1]/td[2]/div/div/div/input")).sendKeys(shipNo);
+//	   // System.out.println(shipNo);  
+//	    
+//	}
 	
     @BeforeMethod
 	public void setup() throws InterruptedException
@@ -35,8 +37,11 @@ public class LoginPageTest extends CSMBase{
 	
  //Validate Login with Valid Credentials 
 	@Test(priority=1)
-	public void LoginWithValidCredentials() throws InterruptedException
+	public void LoginWithValidCredentials() throws InterruptedException, EncryptedDocumentException, IOException
 	{
+		
+		FileInputStream file =new FileInputStream("D:\\Eclipse_rohit\\PO_Screen_Data.xlsx");
+		Sheet sh= WorkbookFactory.create(file).getSheet("Sheet");
 		
        // homePage=loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
         //homePage=loginPage.login("Admin", "AdminQA1");
@@ -45,9 +50,11 @@ public class LoginPageTest extends CSMBase{
 		String homePagetitle= driver.getTitle();
 		Assert.assertEquals(homePagetitle,"Home Page", "Invalid Credentials");
 		System.out.println("Test 1 passed");
-//		String shipNo=sh.getRow(1).getCell(1).getStringCellValue();
-//	    //driver.findElement(By.xpath("/html/body/div/div/section[2]/div[1]/form/div[3]/div/div/div/div[6]/div/div/div[1]/div/table/tbody/tr[1]/td[2]/div/div/div/input")).sendKeys(shipNo);
-//	    System.out.println(shipNo);
+	
+        String shipNo=sh.getRow(1).getCell(1).getStringCellValue();
+	    //driver.findElement(By.xpath("/html/body/div/div/section[2]/div[1]/form/div[3]/div/div/div/div[6]/div/div/div[1]/div/table/tbody/tr[1]/td[2]/div/div/div/input")).sendKeys(shipNo);
+	    System.out.println(shipNo);
+	    
         driver.quit();
 	}
 	
